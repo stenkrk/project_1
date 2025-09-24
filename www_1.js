1b8e0d08ff31700fd8a0ec85f63f11c140b17358
Sten Kreek <stenkrk@users.noreply.github.com>
2025-09-24 13:23:39 +0300

    lisatud algelised marsruudid node.js

--- www_1.js
+++ www_1.js
@@ -1,15 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 const dateEt = require("./src/dateTimeET");
-const pageStart = '<!DOCTYPE html>\n<html lang="et">\n<head>\n<meta charset="utf-8">\n<title>Sten Kreek, veebiprogrammeerimine</title>\n</head>\n<body>';
-const pageBody = '<h1>Sten Kreek, veebiprogrammeerimine</h1>\n<p>See leht on loodud <a href="https://www.tlu.ee">Tallinna ülikoolis</a> veebiprogrammeerimise kurusel ja ei oma mõistlikku sisu.</p>\n<p>Algul lihtsalt HTML ja varsti juba Node.Js.</p>\n<hr>';
-const pageEnd = '</body>\n</html>';
+const textRef = "txt/vanasonad.txt";
+const pageStart = '<!DOCTYPE html>\n<html lang="et">\n<head>\n\t<meta charset="utf-8">\n\t<title>Andrus Rinde, veebiprogrammeerimine</title>\n</head>\n<body>';
+const pageBody = '\n\t<h1>Sten Kreek, veebiprogrammeerimine</h1>\n\t<p>See leht on loodud <a href="https://www.tlu.ee">Tallinna Ülikoolis</a> veebiprogrammeerimise kurusel ja ei oma mõistlikku sisu.</p>\n\t<p>Algul lihtsalt HTML ja varsti juba Node.Js.</p>\n\t<hr>';
+const pageEnd = '\n</body>\n</html>';
 
 http.createServer(function(req, res){
 	res.writeHead(200, {"Content-type": "text/html"});
-	//res.write("Juhhei! Läkski käima!");
-	res.write(pageStart);
-	res.write(pageBody);
-	res.write("<p>Täna on " + dateEt.weekDay() + " " + dateEt.longDate()+ "</p>");
-	res.write(pageEnd);
-	return res.end();
+	fs.readFile(textRef, "utf8", (err, data)=>{
+		if(err){
+			res.write(pageStart);
+			res.write(pageBody);
+			res.write("\n\t<p>Täna on " + dateEt.weekDay() + " " + dateEt.longDate() + ".</p><p>Kahjuks tänaseks ühtki vanasõna välja pakkuda pole!</p>");
+			res.write(pageEnd);
+			return res.end();
+		} else {
+			let oldWisdomList = data.split(";");
+			let folkWisdomOutput = "\n\t<ol>";
+			for (let i = 0; i < oldWisdomList.length; i ++){
+				folkWisdomOutput += "\n\t\t<li>" + oldWisdomList[i] + "</li>";
+			}
+			folkWisdomOutput += "\n\t</ol>";
+			res.write(pageStart);
+			res.write(pageBody);
+			res.write("\n\t<p>Täna on " + dateEt.weekDay() + " " + dateEt.longDate() + ".</p>");
+			res.write("\n\t<h2>Valik Eesti vanasõnu</h2>")
+			res.write(folkWisdomOutput);
+			res.write(pageEnd);
+			return res.end();
+		}
+	});
 }).listen(5313);
